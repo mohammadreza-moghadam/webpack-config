4c1969f5e087a9198d98525b9cce4af2d7b3b8a5
mohammadreza-moghadam <mohammadreza.che.eng@gmail.com>
2022-08-13 17:49:59 +0430

    adding babel and source-map to webpack config file

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ const elvenShieldRecipe = {
     ironIngot: 1,
     refinedMoonstone: 4
 }
+
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4,
+}
+
 console.log(elvenShieldRecipe)
+console.log(elvenGauntletsRecipe)
